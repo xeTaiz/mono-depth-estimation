f8eb46d09bdcd1d68416ac868785464911603299
sebastian <sebastian.hartwig@uni-ulm.de>
2020-10-01 14:07:16 +0200

    added bts

--- criteria.py
+++ criteria.py
@@ -627,3 +627,12 @@ class NormalDepthConsensusLoss(nn.Module):
         prod = torch.abs(mul(prod, (-1.0) * thLog(boundary.clamp(min=self.clamp_value))))
 
         return prod.mean()
+
+class silog_loss(nn.Module):
+    def __init__(self, variance_focus):
+        super(silog_loss, self).__init__()
+        self.variance_focus = variance_focus
+
+    def forward(self, depth_est, depth_gt, mask):
+        d = torch.log(depth_est[mask]) - torch.log(depth_gt[mask])
+        return torch.sqrt((d ** 2).mean() - self.variance_focus * (d.mean() ** 2)) * 10.0
---
+++ modules/bts.py
@@ -0,0 +1,93 @@
+import torch
+import pytorch_lightning as pl
+import criteria
+from datasets import nyu_dataloader
+from network import Bts
+from argparse import ArgumentParser
+import visualize
+from metrics import MetricLogger
+
+class BtsModule(pl.LightningModule):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        
+        self.train_loader = torch.utils.data.DataLoader(nyu_dataloader.NYUDataset(args.path, split='train', output_size=(416, 544), resize=480),
+                                                    batch_size=args.batch_size, 
+                                                    shuffle=True, 
+                                                    num_workers=args.worker, 
+                                                    pin_memory=True)
+        self.val_loader = torch.utils.data.DataLoader(nyu_dataloader.NYUDataset(args.path, split='val', output_size=(416, 544), resize=480),
+                                                    batch_size=1, 
+                                                    shuffle=False, 
+                                                    num_workers=args.worker, 
+                                                    pin_memory=True)
+        self.skip = len(self.val_loader) // 9
+        print("=> creating Model")
+        self.model = Bts.BtsModel(max_depth=self.args.max_depth, bts_size=self.args.bts_size, encoder_version=self.args.encoder)
+        print("=> model created.")
+        self.criterion = criteria.silog_loss(variance_focus=self.args.variance_focus)
+        self.metric_logger = MetricLogger(metrics=['delta1', 'delta2', 'delta3', 'mse', 'mae', 'rmse', 'log10'])
+
+    def forward(self, x):
+        _, _, _, _, y_hat = self.model(x)
+        return y_hat
+
+    def train_dataloader(self):
+        return self.train_loader
+
+    def val_dataloader(self):
+        return self.val_loader
+
+    def training_step(self, batch, batch_idx):
+        if batch_idx == 0: self.metric_logger.reset()
+        x, y = batch
+        y_hat = self(x)
+        mask = y > 0.1
+        loss = self.criterion(y_hat, y, mask.bool())
+        return self.metric_logger.log_train(y_hat, y, loss)
+
+    def validation_step(self, batch, batch_idx):
+        if batch_idx == 0: self.metric_logger.reset()
+        x, y = batch
+        y_hat = self(x)
+        if batch_idx == 0:
+            self.img_merge = visualize.merge_into_row(x, y, y_hat)
+        elif (batch_idx < 8 * self.skip) and (batch_idx % self.skip == 0):
+            row = visualize.merge_into_row(x, y, y_hat)
+            self.img_merge = visualize.add_row(self.img_merge, row)
+        elif batch_idx == 8 * self.skip:
+            filename = "{}/{}/version_{}/epoch{}.jpg".format(self.logger.save_dir, self.logger.name, self.logger.version, self.current_epoch)
+            visualize.save_image(self.img_merge, filename)
+        return self.metric_logger.log_val(y_hat, y, checkpoint_on='mae')
+
+    def configure_optimizers(self):
+        train_param = [{'params': self.model.encoder.parameters(), 'weight_decay': self.args.weight_decay},
+                       {'params': self.model.decoder.parameters(), 'weight_decay': 0}]
+        # Training parameters
+        optimizer = torch.optim.AdamW(train_param, lr=self.args.learning_rate, eps=self.args.adam_eps)
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=self.args.lr_patience)
+        scheduler = {
+            'scheduler': lr_scheduler,
+            'reduce_on_plateua': True,
+            'monitor': 'val_checkpoint_on'
+        }
+        return [optimizer], [scheduler]
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--backbone', default='vgg', type=str, help="Backbone")
+        parser.add_argument('--pretrained', default=1, type=int, help="Use pretrained backbone.")
+        parser.add_argument('--learning_rate', default=0.0001, type=float, help='Learning Rate')
+        parser.add_argument('--batch_size',    default=8,     type=int,   help='Batch Size')
+        parser.add_argument('--worker',        default=6,      type=int,   help='Number of workers for data loader')
+        parser.add_argument('--path', required=True, type=str, help='Path to NYU')
+        parser.add_argument('--lr_patience', default=2, type=int, help='Patience of LR scheduler.')
+        parser.add_argument('--bts_size', type=int, default=512, help='initial num_filters in bts')
+        parser.add_argument('--max_depth', type=int, default=10, help='Depth of decoder')
+        parser.add_argument('--encoder', type=str, default='densenet161_bts', help='Type of encoder')
+        parser.add_argument('--variance_focus', type=float, default=0.85, help='lambda in paper: [0, 1], higher value more focus on minimizing variance of error')
+        parser.add_argument('--adam_eps', type=float, help='epsilon in Adam optimizer', default=1e-6)
+        parser.add_argument('--weight_decay', type=float, help='weight decay factor for optimization', default=1e-2)
+        return parser
