2aad1aafbc8eecf24def72ceede7a32a3753ee65
sebastian <sebastian.hartwig@uni-ulm.de>
2020-10-03 11:39:10 +0200

    update train_laina.py

--- train_laina.py
+++ train_laina.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     parser.add_argument('--seed', default=None, type=int, help='Random Seed')
     parser.add_argument('--precision', default=16,   type=int, help='16 to use Mixed precision (AMP O2), 32 for standard 32 bit float training')
     parser.add_argument('--dev', action='store_true', help='Activate Lightning Fast Dev Run for debugging')
+    parser.add_argument('--gpus', type=int, default=1, help='Number of GPUs')
     parser.add_argument('--overfit', action='store_true', help='If this flag is set the network is overfit to 1 batch')
     parser.add_argument('--min_epochs', default=50, type=int, help='Minimum number of epochs.')
     parser.add_argument('--max_epochs', default=150, type=int, help='Maximum number ob epochs to train')
@@ -27,7 +28,7 @@ if __name__ == "__main__":
         log_gpu_memory=False,
         fast_dev_run=args.dev,
         profiler=True,
-        gpus=1,
+        gpus=args.gpus,
         overfit_batches=1 if args.overfit else 0,
         precision=args.precision,
         amp_level='O2',
