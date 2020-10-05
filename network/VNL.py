f5f738c20fd75e1d4ee259b0775aad3f152ae655
sebastian <sebastian.hartwig@uni-ulm.de>
2020-10-05 17:05:09 +0200

    if pretained is enabled load torchvision weights

--- network/VNL.py
+++ network/VNL.py
@@ -15,22 +15,23 @@ def get_func(args):
     else:
         raise ValueError("Unknown bottom up model")
 
-def load_pretrained_imagenet_resnext_weights(model, weights_file):
+def load_pretrained_imagenet_resnext_weights(model):
     """Load pretrained weights
     Args:
         num_layers: 50 for res50 and so on.
         model: the generalized rcnnn module
     """
-    pretrianed_state_dict = convert_state_dict(torch.load(weights_file))
+    src_dict = dict(torchvision.models.resnext50_32x4d(pretrained=True).named_parameters())
+    pretrianed_state_dict = convert_state_dict(src_dict)
 
     model_state_dict = model.state_dict()
-
+    
     for k, v in pretrianed_state_dict.items():
         if k in model_state_dict.keys():
             model_state_dict[k].copy_(pretrianed_state_dict[k])
         else:
             print('Weight %s is not in ResNeXt model.' % k)
-    logger.info('Pretrained ResNeXt weight has been loaded')
+    print('Pretrained ResNeXt weight has been loaded')
 
 
 def convert_state_dict(src_dict):
@@ -42,20 +43,19 @@ def convert_state_dict(src_dict):
     res_id = 1
     map1 = ['conv1.', 'bn1.', ' ', 'conv2.', 'bn2.']
     map2 = [[' ', 'conv3.', 'bn3.'], ['shortcut.conv.', 'shortcut.bn.']]
+
     for k, v in src_dict.items():
+        if 'fc' in k:continue
+        if 'downsample' in k:continue
         toks = k.split('.')
-        if int(toks[0]) == 0:
-            name = 'res%d.' % res_id + 'conv1.' + toks[-1]
-        elif int(toks[0]) == 1:
-            name = 'res%d.' % res_id + 'bn1.' + toks[-1]
-        elif int(toks[0]) >=4 and int(toks[0]) <= 7:
-            name_res = 'res%d.%d.' % (int(toks[0])-2, int(toks[1]))
-            if len(toks) == 7:
-                name = name_res + map1[int(toks[-2])] + toks[-1]
-            elif len(toks) == 6:
-                name = name_res + map2[int(toks[-3])][int(toks[-2])] + toks[-1]
+        if len(toks) == 6:
+            print(toks)
+        if 'layer' in k:
+            res_id = int(toks[0].replace('layer', '')) + 1 
+            name = "res{}.{}.{}.{}".format(res_id, toks[1], toks[2], toks[3])
         else:
-            continue
+            name = "res1.{}.{}".format(toks[0], toks[1])
+            
         dst_dict[name] = v
 
     return dst_dict
@@ -693,4 +693,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    model = MetricDepthModel(args)
+    model = torchvision.models.resnext50_32x4d(pretrained=True)
+    print(dict(model.named_parameters()).keys())
