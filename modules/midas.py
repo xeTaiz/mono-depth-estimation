852fb8cd2d8412b85e828805f75df30dbf55a243
Sebastian Hartwig <sebastianhartwig@gmx.net>
2020-10-23 18:02:43 +0200

    add alpha flag midas

--- modules/midas.py
+++ modules/midas.py
@@ -114,11 +114,11 @@ class MidasModule(pl.LightningModule):
         self.model = MiDaS.MidasNet(path=weights_path, features=256)
         print("=> model created.")
         if self.args.loss == 'ssimse':
-            self.criterion = criteria.MidasLoss(alpha=0.5, loss='mse')
+            self.criterion = criteria.MidasLoss(alpha=self.args.alpha, loss='mse')
         elif self.args.loss == 'ssitrim':
-            self.criterion = criteria.MidasLoss(alpha=0.5, loss='trimmed')
+            self.criterion = criteria.MidasLoss(alpha=self.args.alpha, loss='trimmed')
         elif self.args.loss == 'ssil1':
-            self.criterion = criteria.MidasLoss(alpha=0.5, loss='l1')
+            self.criterion = criteria.MidasLoss(alpha=self.args.alpha, loss='l1')
         elif self.args.loss == 'eigen':
             self.criterion = criteria.MaskedDepthLoss()
         elif self.args.loss == 'laina':
@@ -210,4 +210,5 @@ class MidasModule(pl.LightningModule):
         parser.add_argument('--dataset', default='nyu', type=str, help='Dataset for Training [nyu, noreflection, isotropic, mirror, structured3d]')
         parser.add_argument('--eval_dataset', default='nyu', type=str, help='Dataset for Validation [nyu, noreflection, isotropic, mirror, structured3d]')
         parser.add_argument('--data_augmentation', default='midas', type=str, help='Choose data Augmentation Strategy: laina or midas')
+        parser.add_argument('--alpha', default=0.5, type=float, help='alpha')
         return parser
