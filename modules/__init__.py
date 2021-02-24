ab27e6170ace8be6315f24791fca84a5b8c66069
Sebastian Hartwig <sebastianhartwig@gmx.net>
2021-02-24 10:42:49 +0100

    fix mymodul

--- modules/__init__.py
+++ modules/__init__.py
@@ -20,7 +20,7 @@ def get_module(args):
     if args.method.name == "dorn":  module = dorn.DORNModule
     if args.method.name == "laina": module = laina.FCRNModule
     if args.method.name == "bts":   module = bts.BtsModule
-    if args.method.name == "my":    module = my.MyNet
+    if args.method.name == "my":    module = my.MyModule
     assert module, "Please select method!"
     if args.method.ckpt: return module.load_from_checkpoint(checkpoint_path=args.method.ckpt, hparams_file=(Path(args.method.ckpt).parents[1]/"hparams.yaml").as_posix(), test=args.test)
     else: return module(args)
