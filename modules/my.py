75057bc2d91fae1ef17bf2fbf24585e8731013e4
Sebastian Hartwig <sebastianhartwig@gmx.net>
2021-02-24 10:14:03 +0100

    my model

---
+++ modules/my.py
@@ -0,0 +1,160 @@
+import torch
+import criteria
+from network import MyNet
+from modules.base_module import BaseModule
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+import numpy as np
+
+def augment_image(image):
+    # gamma augmentation
+    gamma = np.random.uniform(0.9, 1.1)
+    image_aug = image ** gamma
+
+    # brightness augmentation
+    brightness = np.random.uniform(0.75, 1.25)
+    image_aug = image_aug * brightness
+
+    # color augmentation
+    colors = np.random.uniform(0.9, 1.1, size=3)
+    white = np.ones((image.shape[0], image.shape[1]))
+    color_image = np.stack([white * colors[i] for i in range(3)], axis=2)
+    image_aug *= color_image
+    image_aug = np.clip(image_aug, 0, 1)
+
+    return image_aug
+
+class MyModule(BaseModule):
+   
+    def output_size(self):
+        return (384, 384)
+
+    def resize(self):
+        return 400
+
+    def setup_model(self):
+        return MyNet.MyModel(self.output_size())
+
+    def setup_criterion(self):
+        return criteria.MidasLoss(alpha=0.5, loss='mse', reduction='batch-based')
+
+    def forward(self, x):
+        y_hat = self.model(x)
+        return y_hat
+
+    def training_step(self, batch, batch_idx):
+        if batch_idx == 0: self.metric_logger.reset()
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        return self.metric_logger.log_train(y_hat, y, loss)
+
+    def validation_step(self, batch, batch_idx):
+        if batch_idx == 0: self.metric_logger.reset()
+        x, y = batch
+        y_hat = self(x)
+        self.save_visualization(x, y, y_hat, batch_idx)
+        return self.metric_logger.log_val(y_hat, y)
+
+    def test_step(self, batch, batch_idx):
+        if batch_idx == 0: self.metric_logger.reset()
+        x, y = batch
+        x = torch.nn.functional.interpolate(x, self.output_size(), mode='bilinear')
+        y_hat = self(x)
+        y = torch.nn.functional.interpolate(y, (480, 640), mode='bilinear')
+        y_hat = torch.nn.functional.interpolate(y_hat, (480, 640), mode='bilinear')
+        return self.metric_logger.log_test(y_hat, y)
+
+    def configure_optimizers(self):
+        # different modules have different learning rate
+        train_params = [{'params': self.model.get_1x_lr_params(), 'lr': self.method.learning_rate},
+                        {'params': self.model.get_10x_lr_params(), 'lr': self.method.learning_rate * 10}]
+
+        optimizer = torch.optim.Adam(train_params, lr=self.method.learning_rate)
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=self.method.lr_patience)
+        scheduler = {
+            'scheduler': lr_scheduler,
+            'monitor': 'val_delta1'
+        }
+        return [optimizer], [scheduler]
+
+    def train_preprocess(self, rgb, depth):
+        if isinstance(rgb, np.ndarray):
+            rgb = transforms.ToPILImage()(rgb)
+        if isinstance(depth, np.ndarray):
+            depth = transforms.ToPILImage()(depth)
+
+        height = rgb.height
+        width = rgb.width
+        left_margin = width * 0.05
+        right_margin = width * (1.0 - 0.05)
+        top_margin = height * 0.05
+        bot_margin = height * (1.0 - 0.05)
+        depth = depth.crop((left_margin, top_margin, right_margin, bot_margin))
+        rgb     = rgb.crop((left_margin, top_margin, right_margin, bot_margin))
+
+        
+        # Random rotation
+        angle = transforms.RandomRotation.get_params([-2.5, 2.5])
+        rgb = TF.rotate(rgb, angle)
+        depth = TF.rotate(depth, angle)
+
+        # Resize
+        h = int(np.random.choice([416, 452, 489, 507, 518, 550, 600, 650, 720]))
+        resize = transforms.Resize(h)
+        rgb = resize(rgb)
+        depth = resize(depth)
+
+
+        # Random Crop
+        i, j, h, w = transforms.RandomCrop.get_params(rgb, output_size=self.output_size())
+        rgb = TF.crop(rgb, i, j, h, w)
+        depth = TF.crop(depth, i, j, h, w)
+
+        # Random flipping
+        if np.random.uniform(0,1) > 0.5:
+            rgb = TF.hflip(rgb)
+            depth = TF.hflip(depth)
+
+        rgb = np.asarray(rgb, dtype=np.float32) / 255.0
+        depth = np.asarray(depth, dtype=np.float32)
+
+        # Random gamma, brightness, color augmentation
+        if np.random.uniform(0,1) > 0.5:
+            rgb = augment_image(rgb)
+
+        rgb = TF.to_tensor(np.array(rgb))
+        depth = TF.to_tensor(np.array(depth))
+        return rgb, depth
+
+    def val_preprocess(self, rgb, depth):
+        if isinstance(rgb, np.ndarray):
+            rgb = transforms.ToPILImage()(rgb)
+        if isinstance(depth, np.ndarray):
+            depth = transforms.ToPILImage()(depth)
+        # Resize
+        resize = transforms.Resize(self.resize())
+        rgb = resize(rgb)
+        depth = resize(depth)
+        # Center crop
+        crop = transforms.CenterCrop(self.output_size())
+        rgb = crop(rgb)
+        depth = crop(depth)
+        
+        rgb = TF.to_tensor(np.array(rgb, dtype=np.float32))
+        depth = TF.to_tensor(np.array(depth, dtype=np.float32))
+        
+        rgb /= 255.0
+        return rgb, depth
+
+    def test_preprocess(self, rgb, depth):        
+        rgb = TF.to_tensor(np.array(rgb, dtype=np.float32))
+        depth = TF.to_tensor(np.array(depth, dtype=np.float32))
+        rgb /= 255.0
+        return rgb, depth
+
+    @staticmethod
+    def add_model_specific_args(subparsers):
+        parser = subparsers.add_parser('my', help='MyModel specific parameters')
+        BaseModule.add_default_args(parser, name="my", learning_rate=0.0001, batch_size=16)
+        return parser
---
+++ network/MyNet.py
@@ -0,0 +1,253 @@
+import torch
+import torch.nn as nn
+
+class GlobalConsitency(nn.Module):
+    def __init__(self, channels, input_size=(384, 384), out_feat=64):
+        super().__init__()
+        self.inc = nn.Upsample(scale_factor=2)
+        self.avg = nn.AdaptiveMaxPool2d((input_size[0] // 2, input_size[1] // 2))
+        self.conv = nn.Conv2d(channels, channels // 2, kernel_size=3, bias=False, padding=1, stride=1)
+        self.conv_final = nn.Conv2d(channels // 2, out_feat, kernel_size=3, bias=False, padding=1, stride=1)
+
+    def forward(self, x0, x1):
+        x1 = self.inc(x1)
+        x0 = self.avg(x0)
+        x1 = self.avg(x1)
+        x = torch.cat([x0, x1], dim=1)
+        x = self.conv(x)
+        x = self.conv_final(x)
+        return x
+
+class Details(nn.Module):
+    def __init__(self, channels, input_size=(384,384), scale=2, out_feat=64):
+        super().__init__()
+        self.c = int(channels / (scale * scale))
+        self.shuffle = nn.PixelShuffle(scale)
+        self.down       = nn.Conv2d(self.c,      self.c *  2,  kernel_size=3, stride=2, padding=1, bias=False)
+        self.conv       = nn.Conv2d(self.c *  4, self.c *  2,  kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2      = nn.Conv2d(self.c *  2, self.c     ,  kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv_final = nn.Conv2d(self.c     ,    out_feat,  kernel_size=3, stride=1, padding=1, bias=False)
+        self.up = nn.Upsample(scale_factor=2)
+
+    def forward(self, x0, x1):
+        x0 = self.shuffle(x0)
+        x0 = self.down(x0)
+        x1 = self.shuffle(x1)
+        x = torch.cat([x0, x1], dim=1)
+        x = self.conv(x)
+        x = self.conv2(x)
+        x = self.conv_final(x)
+        x = self.up(x)
+        return x
+
+class Sharpness(nn.Module):
+    def __init__(self, channels, out_feat=64):
+        super().__init__()
+        self.conv1 = nn.Conv2d(channels[0], channels[0] * 2, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(channels[1], channels[1] * 2, kernel_size=3, stride=2, padding=1)
+
+        self.tconv1 = nn.ConvTranspose2d(channels[0] * 4 + channels[1] * 2 + channels[2], out_channels=channels[0] * 4 + channels[1] * 2, kernel_size=4, stride=2, padding=1, bias=False)
+        self.tconv2 = nn.ConvTranspose2d(channels[0] * 4 + channels[1] * 2, out_channels=channels[0] * 4, kernel_size=4, stride=2, padding=1, bias=False)
+        self.tconv3 = nn.ConvTranspose2d(channels[0] * 4, out_channels=out_feat, kernel_size=4, stride=2, padding=1, bias=False)
+        self.tconv_final = nn.ConvTranspose2d(out_feat, out_feat, kernel_size=4, stride=2, padding=1, bias=False)
+
+    def forward(self, x0, x1, x2):
+        x0 = self.conv1(x0)
+        x0 = self.conv2(x0)
+        x1 = self.conv2(x1)
+        x = torch.cat([x0, x1, x2], dim=1)
+        x = self.tconv1(x)
+        x = self.tconv2(x)
+        x = self.tconv3(x)
+        x = self.tconv_final(x)
+        return x
+
+class Weighter(nn.Module):
+    def __init__(self, input_size, in_feat):
+        super().__init__()
+        self.conv = nn.Conv2d(in_feat, in_feat // 2, kernel_size=3, stride=2, padding=1, bias=False)
+        self.weights = nn.Linear(input_size[0] * input_size[1] // 16, 3)
+
+    def forward(self, x):
+        [a,b,c] = x
+        
+        a = self.conv(a)
+        b = self.conv(b)
+        c = self.conv(c)
+
+        x = torch.cat([a, b, c], dim=1)
+        x = torch.flatten(x, start_dim=2)
+        x = self.weights(x)
+        #x = x.sigmoid()
+        x = torch.mean(x, dim=1)
+        x = x / torch.sum(x)
+        return x
+
+
+class my_decoder(nn.Module):
+    def __init__(self, input_size, encoder_feature_sizes):
+        super().__init__()
+        self.refine0 = FeatureFusionBlock(encoder_feature_sizes[0])
+        self.refine1 = FeatureFusionBlock(encoder_feature_sizes[1])
+        self.refine2 = FeatureFusionBlock(encoder_feature_sizes[2])
+        self.refine3 = FeatureFusionBlock(encoder_feature_sizes[3])
+
+        self.global_con = GlobalConsitency(encoder_feature_sizes[0] + encoder_feature_sizes[1], input_size=input_size, out_feat=64)
+        self.details = Details(encoder_feature_sizes[1], out_feat=64)
+        self.sharpness = Sharpness([encoder_feature_sizes[2], encoder_feature_sizes[3], encoder_feature_sizes[4]], out_feat=64)
+
+        self.weighter = Weighter(input_size=input_size, in_feat=64)    
+
+        self.get_depth = nn.Sequential(nn.Upsample(scale_factor=2),nn.Conv2d(64, 1, 3, 1, 1, bias=False), nn.Sigmoid())    
+
+    def forward(self, features):
+        skip0, skip1, skip2, skip3 = features[1], features[2], features[3], features[4]
+        dense_features = torch.nn.ReLU()(features[5])
+        x0 = self.refine0(skip0)
+        x1 = self.refine1(skip1)
+        x2 = self.refine2(skip2)
+        x3 = self.refine3(skip3)
+
+        glob = self.global_con(x0, x1)
+        detail = self.details(x1, x2)
+        sharpness = self.sharpness(x2, x3, dense_features)
+
+        glob_d = self.get_depth(glob)
+        detail_d = self.get_depth(detail)
+        sharpness_d = self.get_depth(sharpness)
+
+        scale = self.weighter([glob, detail, sharpness])
+    
+        x = torch.cat([glob_d.unsqueeze(1), detail_d.unsqueeze(1), sharpness_d.unsqueeze(1)], dim=1)
+        x = x * scale[:,:,None, None, None]
+        x = torch.sum(x, dim=1)
+        return x
+
+class encoder(nn.Module):
+    def __init__(self, version):
+        super(encoder, self).__init__()
+        import torchvision.models as models
+        if version == 'densenet121_bts':
+            self.base_model = models.densenet121(pretrained=True).features
+            self.feat_names = ['relu0', 'pool0', 'transition1', 'transition2', 'norm5']
+            self.feat_out_channels = [64, 64, 128, 256, 1024]
+        elif version == 'densenet161_bts':
+            self.base_model = models.densenet161(pretrained=True).features
+            self.feat_names = ['relu0', 'pool0', 'transition1', 'transition2', 'norm5']
+            self.feat_out_channels = [96, 96, 192, 384, 2208]
+        elif version == 'resnet50_bts':
+            self.base_model = models.resnet50(pretrained=True)
+            self.feat_names = ['relu', 'layer1', 'layer2', 'layer3', 'layer4']
+            self.feat_out_channels = [64, 256, 512, 1024, 2048]
+        elif version == 'resnet101_bts':
+            self.base_model = models.resnet101(pretrained=True)
+            self.feat_names = ['relu', 'layer1', 'layer2', 'layer3', 'layer4']
+            self.feat_out_channels = [64, 256, 512, 1024, 2048]
+        elif version == 'resnext50_bts':
+            self.base_model = models.resnext50_32x4d(pretrained=True)
+            self.feat_names = ['relu', 'layer1', 'layer2', 'layer3', 'layer4']
+            self.feat_out_channels = [64, 256, 512, 1024, 2048]
+        elif version == 'resnext101_bts':
+            self.base_model = models.resnext101_32x8d(pretrained=True)
+            self.feat_names = ['relu', 'layer1', 'layer2', 'layer3', 'layer4']
+            self.feat_out_channels = [64, 256, 512, 1024, 2048]
+        else:
+            print('Not supported encoder: {}'.format(version))
+
+    def forward(self, x):
+        features = [x]
+        skip_feat = [x]
+        for k, v in self.base_model._modules.items():
+            if 'fc' in k or 'avgpool' in k:
+                continue
+            feature = v(features[-1])
+            features.append(feature)
+            if any(x in k for x in self.feat_names):
+                skip_feat.append(feature)
+        
+        return skip_feat
+
+class ResidualConvUnit(nn.Module):
+    """Residual convolution module.
+    """
+
+    def __init__(self, features):
+        """Init.
+        Args:
+            features (int): number of features
+        """
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(
+            features, features, kernel_size=3, stride=1, padding=1, bias=True
+        )
+
+        self.conv2 = nn.Conv2d(
+            features, features, kernel_size=3, stride=1, padding=1, bias=True
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        """Forward pass.
+        Args:
+            x (tensor): input
+        Returns:
+            tensor: output
+        """
+        out = self.relu(x)
+        out = self.conv1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+
+        return out + x
+
+class FeatureFusionBlock(nn.Module):
+    """Feature fusion block.
+    """
+
+    def __init__(self, features):
+        """Init.
+        Args:
+            features (int): number of features
+        """
+        super(FeatureFusionBlock, self).__init__()
+
+        self.resConfUnit1 = ResidualConvUnit(features)
+        self.resConfUnit2 = ResidualConvUnit(features)
+
+    def forward(self, *xs):
+        """Forward pass.
+        Returns:
+            tensor: output
+        """
+        output = xs[0]
+
+        if len(xs) == 2:
+            output += self.resConfUnit1(xs[1])
+
+        output = self.resConfUnit2(output)
+
+        #output = nn.functional.interpolate(
+        #    output, scale_factor=2, mode="bilinear", align_corners=True
+        #)
+
+        return output
+
+class MyModel(nn.Module):
+    def __init__(self, input_size=(384, 384), encoder_version='densenet161_bts'):
+        super(MyModel, self).__init__()
+        self.encoder = encoder(encoder_version)
+        self.decoder = my_decoder(input_size, self.encoder.feat_out_channels)
+
+    def forward(self, x):
+        skip_feat = self.encoder(x)
+        return self.decoder(skip_feat)
+
+
+if __name__ == "__main__":
+    input_size = (384, 384)
+    model = MyModel(input_size=input_size)
+    img = torch.rand((2,3, input_size[0], input_size[1]))
+    y_hat = model(img)
+    print(y_hat.shape)
