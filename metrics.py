7f83699576c09cc714d4931879b7b0ace2e52f3e
Sebastian Hartwig <sebastianhartwig@gmx.net>
2021-07-05 12:27:53 +0200

    update to pytorch lightning 1.9 and pytorch_lightning 1.3

--- metrics.py
+++ metrics.py
@@ -5,16 +5,14 @@
 
 
 import torch
-import math
-import numpy as np
-import pytorch_lightning as pl
-from pytorch_lightning.metrics.metric import Metric
+import torchmetrics
+
 
 class MetricLogger(object):
     def __init__(self, metrics, module):
         self.context = module
         self.computer = MetricComputation(metrics)
-    
+
     def log_train(self, pred, target, loss):
         values = self.computer.compute(pred, target)
         result = {"loss": loss}
@@ -45,6 +43,7 @@ class MetricLogger(object):
     def reset(self):
         self.computer.reset()
 
+
 class MetricComputation(object):
     def __init__(self, metrics):
         self.names = metrics
@@ -70,59 +69,52 @@ class MetricComputation(object):
         if isinstance(metric, str): return self.sum[self.names.index(metric)] / self.count
         assert False, "metric must be int or str"
 
-class Delta(Metric):
-    def __init__(self, exp=1, *args, **kwargs):
-        super(Delta, self).__init__(*args, **kwargs)
-        self.exp = exp
-
-    def forward(self, pred, target):
-        maxRatio = torch.max(pred / target, target / pred)
-        return (maxRatio < 1.25 ** self.exp).float().mean()
-
-class Log10(Metric):
-    def log10(self, x):
-        return torch.log(x) / torch.log(torch.tensor(10.0))
-    def forward(self, pred, target):
-        return (self.log10(pred) - self.log10(target)).abs().mean()
 
 def Delta1_multi_gpu(pred, target):
     maxRatio = torch.max(pred / target, target / pred)
     return (maxRatio < 1.25 ** 1).float().mean()
 
+
 def Delta2_multi_gpu(pred, target):
     maxRatio = torch.max(pred / target, target / pred)
     return (maxRatio < 1.25 ** 2).float().mean()
+
 
 def Delta3_multi_gpu(pred, target):
     maxRatio = torch.max(pred / target, target / pred)
     return (maxRatio < 1.25 ** 3).float().mean()
 
+
 def Log10_multi_gpu(pred, target):
     return (torch.log10(pred) - torch.log10(target)).abs().mean()
+
 
 def AbsoluteRelativeError(pred, target):
     if (target == 0).any():
         raise NotComputableError("The ground truth has 0.")
     return (torch.abs(pred - target) / target).mean()
 
+
 def RelativeSquareError(pred, target):
     if (target == 0).any():
         raise NotComputableError("The ground truth has 0.")
     return ((pred - target)**2 / target).mean()
+
 
 def RelativeMeanSquareError(pred, target):
     if (target == 0).any():
         raise NotComputableError("The ground truth has 0.")
     return torch.sqrt((pred - target)**2 / target).mean()
 
-METRICS = pl.metrics.functional.__dict__
-METRICS['mse'] = METRICS['mean_squared_error']
-METRICS['msle'] = METRICS['mean_squared_log_error']
-METRICS['mae'] = METRICS['mean_absolute_error']
-METRICS['delta1'] = Delta1_multi_gpu#Delta(exp=1, name="delta1")
-METRICS['delta2'] = Delta2_multi_gpu#Delta(exp=2, name="delta2")
-METRICS['delta3'] = Delta3_multi_gpu#Delta(exp=3, name="delta3")
-METRICS['log10'] = Log10_multi_gpu#Log10(name="log10")
+
+METRICS = {}
+METRICS['mse'] = torchmetrics.functional.regression.mean_squared_error
+METRICS['msle'] = torchmetrics.functional.regression.mean_squared_log_error
+METRICS['mae'] = torchmetrics.functional.regression.mean_absolute_error
+METRICS['delta1'] = Delta1_multi_gpu  # Delta(exp=1, name="delta1")
+METRICS['delta2'] = Delta2_multi_gpu  # Delta(exp=2, name="delta2")
+METRICS['delta3'] = Delta3_multi_gpu  # Delta(exp=3, name="delta3")
+METRICS['log10'] = Log10_multi_gpu    # Log10(name="log10")
 METRICS['absrel'] = AbsoluteRelativeError
 METRICS['sqrel'] = RelativeSquareError
 METRICS['rmse'] = RelativeMeanSquareError
