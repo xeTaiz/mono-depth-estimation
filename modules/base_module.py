e1ca5ec412f07be8a85801e271269227b0a583b1
Dominik Engel <dominik.engel@mail.de>
2022-01-31 16:07:16 +0100

    add image log to tensorboard

--- modules/base_module.py
+++ modules/base_module.py
@@ -172,6 +172,7 @@ class BaseModule(pl.LightningModule):
         elif batch_idx == 8 * self.skip:
             filename = "{}/{}/version_{}/epoch{}.jpg".format(self.logger.save_dir, self.logger.name, self.logger.version, self.current_epoch)
             visualize.save_image(self.img_merge, filename)
+            self.logger.experiment.log_image(self.img_merge, self.log_step)
 
     def get_dataset(self):
         training_dataset = []
