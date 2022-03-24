d370dcb57a1d644ab434df113ddd7782bdc18825
Dominik Engel <dominik.engel@mail.de>
2022-03-24 15:28:18 +0100

    add NpyDataset to infer

--- infer.py
+++ infer.py
@@ -6,12 +6,29 @@ import pytorch_lightning as pl
 from train import parse_args_into_namespaces
 from pathlib import Path
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import torchvision.transforms.functional as TF
+
+import numpy as np
+
+class NpyDataset(torch.utils.data.Dataset):
+    def __init__(self, path):
+        self.path = Path(path)
+        self.items = self.path.rglob('*.npy')
+
+    def __len__(self): return len(self.items)
+
+    def __getitem__(self, i):
+        item = np.load(self.items[i])
+        rgb = TF.to_tensor(item['rgb'])
+        gt = TF.to_tensor(item['gt'])
+        return rgb, gt
 
 if __name__ == "__main__":
     parser = ArgumentParser('Trains mono depth estimation models', formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('--gpus', type=int, default=1, help='Number of GPUs')
     parser.add_argument('--metrics', default=['delta1', 'delta2', 'delta3', 'mse', 'mae', 'log10', 'rmse'], nargs='+', help='which metrics to evaluate')
     parser.add_argument('--worker', default=6, type=int, help='Number of workers for data loader')
+    parser.add_argument('--inputs', type=str, default=None, help='Path to .npy files')
 
     commands = parser.add_subparsers(title='Commands')
     register_dataset_specific_arguments(commands)
@@ -38,7 +55,12 @@ if __name__ == "__main__":
     # Fit model
     module = get_module(args)
     module.pred_path = pred_path
-    result = trainer.test(module, verbose=False)
-    for key, value in result[0].items():
-        if "_epoch" in key:
-            print("{}: {}".format(key.replace("_epoch", ""), round(value, 3)))
+    if args.inputs:
+        ds = NpyDataset(args.inputs)
+        dl = torch.utils.data.Dataloader(ds, batch_size=1)
+        trainer.test(module, dl)
+    else:
+        result = trainer.test(module, verbose=False)
+        for key, value in result[0].items():
+            if "_epoch" in key:
+                print("{}: {}".format(key.replace("_epoch", ""), round(value, 3)))
